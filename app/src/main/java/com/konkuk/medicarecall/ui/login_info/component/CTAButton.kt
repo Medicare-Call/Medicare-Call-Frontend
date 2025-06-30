b8596f05c61f63130e89a1c3b1bcf95e26bf0886
ProtossManse <ingan2473@gmail.com>
2025-07-01 01:21:18 +0900

    Feat: CTA 버튼 & 로그인 시작하기 화면 구현

---
+++ app/src/main/java/com/konkuk/medicarecall/ui/login_info/component/CTAButton.kt
@@ -0,0 +1,91 @@
+package com.konkuk.medicarecall.ui.login_info.component
+
+import android.R.attr.text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.konkuk.medicarecall.ui.theme.MediCareCallTheme
+
+enum class CTAButtonType {
+    WHITE, GREEN
+}
+
+
+@Composable
+// CTAButtonType을 이용해 흰 버튼인지 초록 버튼인지 정해지고,
+// 클릭 시에 버튼 색상이 바뀜.
+fun CTAButton(
+    color: CTAButtonType,
+    text: String,
+    callback: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+
+    val backgroundColor = when (color) {
+        CTAButtonType.WHITE -> MediCareCallTheme.colors.white
+        CTAButtonType.GREEN -> MediCareCallTheme.colors.main
+    }
+
+    val textColor = when (color) {
+        CTAButtonType.WHITE -> MediCareCallTheme.colors.main
+        CTAButtonType.GREEN -> MediCareCallTheme.colors.white
+    }
+
+    val clickedColor = when (color) {
+        CTAButtonType.WHITE -> MediCareCallTheme.colors.gray1
+        CTAButtonType.GREEN -> MediCareCallTheme.colors.g500
+    }
+
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    // isPressed 상태에 따라 버튼 색상 결정
+    val buttonColor = if (isPressed) clickedColor else backgroundColor
+
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .padding(horizontal = 20.dp)
+            .background(color = buttonColor, shape = RoundedCornerShape(14.dp))
+            .clickable(
+                interactionSource,
+                onClick = callback,
+                indication = null,
+            )
+    ) {
+        Text(
+            text = text,
+            color = textColor,
+            style = MediCareCallTheme.typography.B_17,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun CTAButtonPreview() {
+    CTAButton(CTAButtonType.GREEN, "시작하기", {})
+}
--- app/src/main/java/com/konkuk/medicarecall/ui/login_info/screen/LoginStartScreen.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/login_info/screen/LoginStartScreen.kt
@@ -1,9 +1,51 @@
 package com.konkuk.medicarecall.ui.login_info.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.konkuk.medicarecall.R
+import com.konkuk.medicarecall.ui.login_info.component.CTAButton
+import com.konkuk.medicarecall.ui.login_info.component.CTAButtonType
+import com.konkuk.medicarecall.ui.theme.MediCareCallTheme
 
 @Composable
 fun LoginStartScreen(modifier: Modifier = Modifier) {
-    
+    Box(
+        modifier
+            .fillMaxSize()
+            .background(MediCareCallTheme.colors.main)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.bg_login_start),
+            "로그인 시작 배경 이미지",
+            modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+
+        )
+
+        CTAButton(
+            color = CTAButtonType.WHITE,
+            "시작하기",
+            {},
+            modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 20.dp)
+        )
+
+    }
+}
+
+@Preview
+@Composable
+private fun LoginStartScreenPreview() {
+    LoginStartScreen()
 }
