17dd7300e0e949951e6d1cbc87f5bee6b002c742
ProtossManse <ingan2473@gmail.com>
2025-07-01 15:52:40 +0900

    Feat: LoginViewModel 추가
    로그인 상태 관리

--- app/src/main/java/com/konkuk/medicarecall/ui/login_info/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/login_info/viewmodel/LoginViewModel.kt
@@ -1,8 +1,39 @@
 package com.konkuk.medicarecall.ui.login_info.viewmodel
 
+import androidx.compose.runtime.MutableState
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.konkuk.medicarecall.data.model.LoginState
+import com.konkuk.medicarecall.data.model.LoginUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 
-class LoginViewModel: ViewModel() {
+class LoginViewModel : ViewModel() {
 
+    val isLoggedIn = true // 추후 서버나 로컬에서 정보 받아오기
+
+    private val _loginState = MutableStateFlow<LoginState>(LoginState.Loading)
+    val loginState: StateFlow<LoginState> = _loginState.asStateFlow()
+
+    private val _loginUiState = MutableStateFlow<LoginUiState>(LoginUiState.Start)
+    val loginUiState: StateFlow<LoginUiState> = _loginUiState.asStateFlow()
+
+    init {
+        checkLoginStatus()
+    }
+
+    fun checkLoginStatus() {
+        viewModelScope.launch {
+            _loginState.value = LoginState.Loading
+        }
+        if (isLoggedIn) {
+            _loginState.value = LoginState.LoggedIn
+        } else {
+            _loginState.value = LoginState.NotLoggedIn
+        }
+
+    }
 }
