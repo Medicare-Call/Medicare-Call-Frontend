59c62f79f2e4a8977e92e9ed0ca9927c84289221
librawish808 <sophiadiary842@gmail.com>
2025-09-29 14:42:29 +0900

    Refactor: #123 AuthService 통합 -TokenRefreshService.kt -VerificationService.kt

---
+++ app/src/main/java/com/konkuk/medicarecall/data/api/auth/AuthService.kt
@@ -0,0 +1,24 @@
+package com.konkuk.medicarecall.data.api.auth
+
+import com.konkuk.medicarecall.data.dto.request.CertificationCodeRequestDto
+import com.konkuk.medicarecall.data.dto.request.PhoneNumberConfirmRequestDto
+import com.konkuk.medicarecall.data.dto.request.TokenRefreshRequestDto
+import com.konkuk.medicarecall.data.dto.response.MemberTokenResponseDto
+import com.konkuk.medicarecall.data.dto.response.VerificationResponseDto
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthService {
+    @POST("auth/refresh")
+    suspend fun refreshToken(@Body req: TokenRefreshRequestDto)
+        : Response<MemberTokenResponseDto>
+
+    @POST("verifications")
+    suspend fun requestCertificationCode(@Body req: CertificationCodeRequestDto)
+        : Response<Unit>
+
+    @POST("verifications/confirmation")
+    suspend fun confirmPhoneNumber(@Body req: PhoneNumberConfirmRequestDto)
+        : Response<VerificationResponseDto>
+}
