c2ac63bedeb5932f68c897fb842b8fc4a0d7686b
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-09-29 16:11:45 +0900

    Refactor: #113 Route sealed class 에서 sealed interface 로 변경

--- app/src/main/java/com/konkuk/medicarecall/ui/navigation/MainNavigator.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/navigation/MainNavigator.kt
@@ -17,7 +17,7 @@ class MainNavigator(
     private val currentDestination: NavDestination?
         @Composable get() = navController
             .currentBackStackEntryAsState().value?.destination
-    val startDestination = Route.AppSplash.route
+    val startDestination = Route.AppSplash
 
     val currentTab: MainTab?
         @Composable get() = MainTab.find { tab ->
--- app/src/main/java/com/konkuk/medicarecall/ui/navigation/Route.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/navigation/Route.kt
@@ -2,49 +2,111 @@ package com.konkuk.medicarecall.ui.navigation
 
 import kotlinx.serialization.Serializable
 
-sealed class Route(val route: String) {
-    object AppSplash : Route("app_splash")
-    object LoginStart : Route("login_start")
-    object LoginPhone : Route("login_phone")
-    object LoginVerification : Route("login_verification")
-    object LoginMyInfo : Route("login_my_info")
-    object LoginElderInfoScreen : Route("login_elder_info")
-    object LoginElderMedInfoScreen : Route("login_elder_med_info")
-    object SetCall : Route("set_call")
-    object Payment : Route("payment")
-    object NaverPay : Route("naver_pay")
-    object NaverPayWithCode : Route("naver_pay/{orderCode}") {
-        fun create(orderCode: String) = "naver_pay/$orderCode"
-    }
+sealed interface Route {
+    @Serializable
+    data object AppSplash : Route
 
-    object FinishSplash : Route("finish_splash")
-    object Home : Route("home")
-    object Statistics : Route("statistics")
-    object Settings : Route("settings")
+    @Serializable
+    data object LoginStart : Route
 
-    object Alarm : Route("alarm")
+    @Serializable
+    data object LoginPhone : Route
 
-    object Announcement : Route("announcement")
-    object AnnouncementDetail : Route("announcement_detail")
-    object HealthInfo : Route("health_info")
-    object HealthDetail : Route("health_detail")
-    object MyDataSetting : Route("my_data_setting")
-    object MyDetail : Route("my_detail")
-    object PersonalDetail : Route("personal_detail")
-    object PersonalInfo : Route("personal_info")
-    object ServiceCenter : Route("service_center")
-    object SettingAlarm : Route("setting_alarm")
-    object SettingSubscribe : Route("setting_subscribe")
-    object SubscribeDetail : Route("subscribe_detail")
-    object MealDetail : Route("home_meal_detail")
-    object MedicineDetail : Route("home_medicine_detail")
-    object SleepDetail : Route("home_sleep_detail")
-    object StateHealthDetail : Route("home_state_health_detail")
-    object StateMentalDetail : Route("home_state_mental_detail")
-    object GlucoseDetail : Route("home_glucose_detail")
+    @Serializable
+    data object LoginVerification : Route
+
+    @Serializable
+    data object LoginMyInfo : Route
+
+    @Serializable
+    data object LoginElderInfoScreen : Route
+
+    @Serializable
+    data object LoginElderMedInfoScreen : Route
+
+    @Serializable
+    data object SetCall : Route
+
+    @Serializable
+    data object Payment : Route
+
+    @Serializable
+    data object NaverPay : Route
+
+    @Serializable
+    data class NaverPayWithCode(val orderCode: String) : Route
+
+    @Serializable
+    data object FinishSplash : Route
+
+    @Serializable
+    data object Home : Route
+
+    @Serializable
+    data object Statistics : Route
+
+    @Serializable
+    data object Settings : Route
+
+    @Serializable
+    data object Alarm : Route
+
+    @Serializable
+    data object Announcement : Route
+
+    @Serializable
+    data object AnnouncementDetail : Route
+
+    @Serializable
+    data object HealthInfo : Route
+
+    @Serializable
+    data object HealthDetail : Route
+
+    @Serializable
+    data object MyDataSetting : Route
+
+    @Serializable
+    data object MyDetail : Route
+
+    @Serializable
+    data object PersonalDetail : Route
+
+    @Serializable
+    data object PersonalInfo : Route
+
+    @Serializable
+    data object ServiceCenter : Route
+
+    @Serializable
+    data object SettingAlarm : Route
+
+    @Serializable
+    data object SettingSubscribe : Route
+
+    @Serializable
+    data object SubscribeDetail : Route
+
+    @Serializable
+    data object MealDetail : Route
+
+    @Serializable
+    data object MedicineDetail : Route
+
+    @Serializable
+    data object SleepDetail : Route
+
+    @Serializable
+    data object StateHealthDetail : Route
+
+    @Serializable
+    data object StateMentalDetail : Route
+
+    @Serializable
+    data object GlucoseDetail : Route
 }
 
-sealed interface MainTabRoute {
+sealed interface MainTabRoute : Route {
     @Serializable
     data object DailySummary : MainTabRoute
 
