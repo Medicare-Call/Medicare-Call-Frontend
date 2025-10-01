cfcdb9e015d3e95d5634583e2e0703ef219f8695
ProtossManse <ingan2473@gmail.com>
2025-10-01 23:57:59 +0900

    Refactor: ElderInputForm에 ViewModel 전달하는 문제 수정
    - 오로지 이벤트만 전달하는 것으로 수정.

--- app/src/main/java/com/konkuk/medicarecall/ui/feature/login/senior/component/ElderInputForm.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/feature/login/senior/component/ElderInputForm.kt
@@ -1,178 +1,137 @@
 package com.konkuk.medicarecall.ui.feature.login.senior.component
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.konkuk.medicarecall.ui.common.component.DefaultDropdown
 import com.konkuk.medicarecall.ui.common.component.DefaultTextField
 import com.konkuk.medicarecall.ui.common.component.GenderToggleButton
-import com.konkuk.medicarecall.ui.feature.login.senior.LoginElderViewModel
-import com.konkuk.medicarecall.ui.type.ElderResidenceType
-import com.konkuk.medicarecall.ui.type.RelationshipType
-import com.konkuk.medicarecall.ui.theme.MediCareCallTheme
-import com.konkuk.medicarecall.ui.theme.figmaShadow
 import com.konkuk.medicarecall.ui.common.util.DateOfBirthVisualTransformation
 import com.konkuk.medicarecall.ui.common.util.PhoneNumberVisualTransformation
+import com.konkuk.medicarecall.ui.model.ElderData
+import com.konkuk.medicarecall.ui.theme.MediCareCallTheme
+import com.konkuk.medicarecall.ui.type.ElderResidenceType
+import com.konkuk.medicarecall.ui.type.RelationshipType
 
 @Composable
 fun ElderInputForm(
-    loginElderViewModel: LoginElderViewModel,
     scrollState: ScrollState,
-    isExpanded: Boolean,
-    index: Int,
-    modifier: Modifier = Modifier
+    elderData: ElderData,
+    onNameChanged: (String) -> Unit,
+    onBirthDateChanged: (String) -> Unit,
+    onGenderChanged: (Boolean) -> Unit,
+    onPhoneNumberChanged: (String) -> Unit,
+    onRelationshipChange: (String) -> Unit,
+    onLivingTypeChanged: (String) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
-    AnimatedVisibility(
-        isExpanded,
-        enter = fadeIn() + expandVertically(),
-        exit = fadeOut() + shrinkVertically()
+    Column(
+        verticalArrangement = Arrangement.Center,
     ) {
-        Column(
-            verticalArrangement = Arrangement.Center
-        ) {
-            DefaultTextField(
-                value = loginElderViewModel.nameList[index],
-                onValueChange = { loginElderViewModel.onNameChanged(index, it) },
-                category = "이름",
-                placeHolder = "이름"
-            )
-            Spacer(Modifier.height(20.dp))
-            DefaultTextField(
-                loginElderViewModel.dateOfBirthList[index],
-                { input ->
-                    val filtered = input.filter { it.isDigit() }.take(8)
-                    loginElderViewModel.onDOBChanged(index, filtered)
-                },
-                category = "생년월일",
-                placeHolder = "YYYY / MM / DD",
-                keyboardType = KeyboardType.Number,
-                visualTransformation = DateOfBirthVisualTransformation(),
-                maxLength = 8
-            )
-            Spacer(Modifier.height(20.dp))
-            Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
-                Text(
-                    "성별",
-                    color = MediCareCallTheme.colors.gray7,
-                    style = MediCareCallTheme.typography.M_17
-                )
-
-                GenderToggleButton(loginElderViewModel.isMaleBoolList[index]) {
-                    loginElderViewModel.onGenderChanged(
-                        index, it
-                    )
-                }
-            }
-            Spacer(Modifier.height(20.dp))
-            DefaultTextField(
-                loginElderViewModel.phoneNumberList[index],
-                { input ->
-                    val filtered = input.filter { it.isDigit() }.take(11)
-                    loginElderViewModel.onPhoneNumberChanged(index, filtered)
-                },
-                category = "휴대폰 번호",
-                placeHolder = "010-1234-5678",
-                keyboardType = KeyboardType.Number,
-                visualTransformation = PhoneNumberVisualTransformation(),
-                maxLength = 11
-            )
-            Spacer(Modifier.height(20.dp))
-
-
-            DefaultDropdown(
-                enumList = RelationshipType.entries.map { it.displayName }
-                    .toList(),
-                placeHolder = "관계 선택하기",
-                category = "어르신과의 관계",
-                scrollState,
-                { loginElderViewModel.onRelationshipChanged(index, it) },
-                loginElderViewModel.relationshipList[index]
+        DefaultTextField(
+            value = elderData.name,
+            onValueChange = { input ->
+                onNameChanged(input)
+            },
+            category = "이름",
+            placeHolder = "이름",
+        )
+        Spacer(Modifier.height(20.dp))
+        DefaultTextField(
+            elderData.birthDate,
+            { input ->
+                val filtered = input.filter { it.isDigit() }.take(8)
+                onBirthDateChanged(filtered)
+            },
+            category = "생년월일",
+            placeHolder = "YYYY / MM / DD",
+            keyboardType = KeyboardType.Number,
+            visualTransformation = DateOfBirthVisualTransformation(),
+            maxLength = 8,
+        )
+        Spacer(Modifier.height(20.dp))
+        Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+            Text(
+                "성별",
+                color = MediCareCallTheme.colors.gray7,
+                style = MediCareCallTheme.typography.M_17,
             )
 
-
-            Spacer(Modifier.height(20.dp))
-
-            DefaultDropdown(
-                enumList = ElderResidenceType.entries.map { it.displayName }
-                    .toList(),
-                placeHolder = "거주방식을 선택해주세요",
-                category = "어르신 거주 방식",
-                scrollState,
-                { loginElderViewModel.onLivingTypeChanged(index, it) },
-                loginElderViewModel.livingTypeList[index]
-            )
-
-            Spacer(Modifier.height(20.dp))
-        }
-    }
-    if (!isExpanded) {
-        Box(
-            Modifier
-                .fillMaxWidth()
-                .figmaShadow(MediCareCallTheme.shadow.shadow03)
-                .clip(
-                    RoundedCornerShape(14.dp)
-                )
-                .background(MediCareCallTheme.colors.white)
-                .clickable { loginElderViewModel.expandedFormIndex = index }
-        ) {
-            Column(
-                Modifier.padding(16.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                Row(
-                    Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Text(
-                        "${loginElderViewModel.nameList[index]} 님",
-                        color = MediCareCallTheme.colors.gray8,
-                        style = MediCareCallTheme.typography.SB_18
-                    )
-                    Text(
-                        "삭제하기",
-                        color = MediCareCallTheme.colors.negative,
-                        style = MediCareCallTheme.typography.R_14,
-                        modifier = Modifier.clickable(
-                            indication = null,
-                            interactionSource = null,
-                            onClick = {
-                                loginElderViewModel.elders--
-                                loginElderViewModel.expandedFormIndex = 0
-                            }
-                        )
-                    )
-                }
-                Text(
-                    loginElderViewModel.phoneNumberList[index].replaceFirst(
-                        Regex("(\\d{3})(\\d{4})(\\d{4})"),
-                        "$1-$2-$3"
-                    ),
-                    color = MediCareCallTheme.colors.gray4,
-                    style = MediCareCallTheme.typography.R_14
-                )
+            GenderToggleButton(elderData.gender) {
+                onGenderChanged(it)
             }
         }
-        Spacer(Modifier.height(30.dp))
+        Spacer(Modifier.height(20.dp))
+        DefaultTextField(
+            elderData.phoneNumber,
+            { input ->
+                val filtered = input.filter { it.isDigit() }.take(11)
+                onPhoneNumberChanged(filtered)
+            },
+            category = "휴대폰 번호",
+            placeHolder = "010-1234-5678",
+            keyboardType = KeyboardType.Number,
+            visualTransformation = PhoneNumberVisualTransformation(),
+            maxLength = 11,
+        )
+        Spacer(Modifier.height(20.dp))
+
+
+        DefaultDropdown(
+            enumList = RelationshipType.entries.map { it.displayName }
+                .toList(),
+            placeHolder = "관계 선택하기",
+            category = "어르신과의 관계",
+            scrollState,
+            { onRelationshipChange(it) },
+            elderData.relationship,
+        )
+
+
+        Spacer(Modifier.height(20.dp))
+
+        DefaultDropdown(
+            enumList = ElderResidenceType.entries.map { it.displayName }
+                .toList(),
+            placeHolder = "거주방식을 선택해주세요",
+            category = "어르신 거주 방식",
+            scrollState,
+            { onLivingTypeChanged(it) },
+            elderData.livingType,
+        )
+
+        Spacer(Modifier.height(20.dp))
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ElderInputFormPreview() {
+    ElderInputForm(
+        scrollState = rememberScrollState(),
+        elderData = ElderData(
+            name = "김어르신",
+            birthDate = "19450101",
+            gender = true,
+            phoneNumber = "01012345678",
+            relationship = "자녀",
+            livingType = "혼자 거주",
+        ),
+        onNameChanged = {},
+        onBirthDateChanged = {},
+        onGenderChanged = {},
+        onPhoneNumberChanged = {},
+        onRelationshipChange = {},
+        onLivingTypeChanged = {},
+    )
 }
--- app/src/main/java/com/konkuk/medicarecall/ui/feature/login/senior/screen/LoginElderInfoScreen.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/feature/login/senior/screen/LoginElderInfoScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -37,24 +38,30 @@ import com.konkuk.medicarecall.R
 import com.konkuk.medicarecall.navigation.Route
 import com.konkuk.medicarecall.ui.common.component.CTAButton
 import com.konkuk.medicarecall.ui.common.component.DefaultSnackBar
-import com.konkuk.medicarecall.ui.feature.login.senior.component.ElderInputForm
-import com.konkuk.medicarecall.ui.feature.login.info.component.LoginBackButton
-import com.konkuk.medicarecall.ui.feature.login.senior.LoginElderViewModel
-import com.konkuk.medicarecall.ui.type.CTAButtonType
-import com.konkuk.medicarecall.ui.theme.MediCareCallTheme
 import com.konkuk.medicarecall.ui.common.util.isValidDate
+import com.konkuk.medicarecall.ui.feature.login.info.component.LoginBackButton
+import com.konkuk.medicarecall.ui.feature.login.senior.component.ElderInputForm
+import com.konkuk.medicarecall.ui.feature.login.senior.LoginElderViewModel
+import com.konkuk.medicarecall.ui.model.ElderData
+import com.konkuk.medicarecall.ui.theme.MediCareCallTheme
+import com.konkuk.medicarecall.ui.type.CTAButtonType
 import kotlinx.coroutines.launch
 
 @Composable
 fun LoginElderScreen(
     navController: NavController,
     loginElderViewModel: LoginElderViewModel,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
 
     val scrollState = rememberScrollState()
     val snackBarState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
+
+    val elderList = loginElderViewModel.elderDataList
+
+    val selectedElderIndex by loginElderViewModel.selectedIndex.collectAsState()
+    val selectedElder = loginElderViewModel.elderDataList[selectedElderIndex]
 
     Box(
         modifier
@@ -65,32 +72,36 @@ fun LoginElderScreen(
             .imePadding(),
     ) {
         Column {
-            LoginBackButton({
-                navController.navigate(Route.LoginElderInfoScreen.route) {
-                    popUpTo(Route.LoginStart.route) { inclusive = false } // ← 스택 정리
-                    launchSingleTop = true
-                    restoreState = true
-                }
-            })
+            LoginBackButton(
+                {
+                    navController.navigate(Route.LoginElderInfoScreen.route) {
+                        popUpTo(Route.LoginStart.route) { inclusive = false } // ← 스택 정리
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+            )
             Column(
                 modifier
-                    .verticalScroll(scrollState)
+                    .verticalScroll(scrollState),
             ) {
                 Spacer(Modifier.height(20.dp))
                 Text(
                     "어르신 등록하기",
                     style = MediCareCallTheme.typography.B_26,
-                    color = MediCareCallTheme.colors.black
+                    color = MediCareCallTheme.colors.black,
                 )
                 Spacer(Modifier.height(30.dp))
-                repeat(loginElderViewModel.elders) { index ->
-                    ElderInputForm(
-                        loginElderViewModel,
-                        scrollState,
-                        index == loginElderViewModel.expandedFormIndex,
-                        index
-                    )
-                }
+                ElderInputForm(
+                    scrollState = scrollState,
+                    elderData = selectedElder,
+                    onNameChanged = { loginElderViewModel.elderDataList[selectedElderIndex] = selectedElder.copy(name = it) },
+                    onBirthDateChanged = { loginElderViewModel.elderDataList[selectedElderIndex] = selectedElder.copy(birthDate = it) },
+                    onGenderChanged = { loginElderViewModel.elderDataList[selectedElderIndex] = selectedElder.copy(gender = it) },
+                    onPhoneNumberChanged = { loginElderViewModel.elderDataList[selectedElderIndex] = selectedElder.copy(phoneNumber = it) },
+                    onRelationshipChange = { loginElderViewModel.elderDataList[selectedElderIndex] = selectedElder.copy(relationship = it) },
+                    onLivingTypeChanged = { loginElderViewModel.elderDataList[selectedElderIndex] = selectedElder.copy(livingType = it) },
+                )
 
 
                 val interactionSource = remember { MutableInteractionSource() }
@@ -108,7 +119,7 @@ fun LoginElderScreen(
                                     MediCareCallTheme.colors.g200
                                 else
                                     MediCareCallTheme.colors.g50
-                            } else MediCareCallTheme.colors.gray1
+                            } else MediCareCallTheme.colors.gray1,
                         )
                         .border(
                             1.2.dp,
@@ -116,62 +127,34 @@ fun LoginElderScreen(
                                 MediCareCallTheme.colors.main
                             else
                                 MediCareCallTheme.colors.gray3,
-                            shape = RoundedCornerShape(14.dp)
+                            shape = RoundedCornerShape(14.dp),
                         )
                         .clickable(
                             enabled = loginElderViewModel.isInputComplete(),
                             indication = null,
-                            interactionSource = interactionSource
+                            interactionSource = interactionSource,
                         ) {
-                            if (loginElderViewModel.elders < 5) {
-                                loginElderViewModel.elders++
-                                loginElderViewModel.expandedFormIndex =
-                                    loginElderViewModel.elders - 1
-                                loginElderViewModel.onNameChanged(
-                                    loginElderViewModel.expandedFormIndex,
-                                    ""
-                                )
-                                loginElderViewModel.onDOBChanged(
-                                    loginElderViewModel.expandedFormIndex,
-                                    ""
-                                )
-                                loginElderViewModel.onRelationshipChanged(
-                                    loginElderViewModel.expandedFormIndex,
-                                    ""
-                                )
-                                loginElderViewModel.onGenderChanged(
-                                    loginElderViewModel.expandedFormIndex,
-                                    null
-                                )
-                                loginElderViewModel.onLivingTypeChanged(
-                                    loginElderViewModel.expandedFormIndex,
-                                    ""
-                                )
-                                loginElderViewModel.onPhoneNumberChanged(
-                                    loginElderViewModel.expandedFormIndex,
-                                    ""
-                                )
-
-
+                            if (loginElderViewModel.elderDataList.size < 5) {
+                                loginElderViewModel.elderDataList.add(ElderData())
                             } else {
                                 coroutineScope.launch {
                                     snackBarState.showSnackbar("어르신은 최대 5명까지 등록이 가능해요")
 
                                 }
                             }
-                        }
+                        },
                 ) {
                     Row(
                         Modifier
                             .padding(vertical = 16.dp)
-                            .align(Alignment.Center)
+                            .align(Alignment.Center),
 
-                    ) {
+                        ) {
                         Icon(
                             painterResource(R.drawable.ic_plus), contentDescription = "플러스 아이콘",
                             tint = if (loginElderViewModel.isInputComplete())
                                 MediCareCallTheme.colors.main
-                            else MediCareCallTheme.colors.gray3
+                            else MediCareCallTheme.colors.gray3,
                         )
                         Spacer(Modifier.width(8.dp))
                         Text(
@@ -179,7 +162,7 @@ fun LoginElderScreen(
                             color = if (loginElderViewModel.isInputComplete())
                                 MediCareCallTheme.colors.main
                             else MediCareCallTheme.colors.gray3,
-                            style = MediCareCallTheme.typography.B_17
+                            style = MediCareCallTheme.typography.B_17,
                         )
                     }
                 }
@@ -191,50 +174,49 @@ fun LoginElderScreen(
                     else CTAButtonType.DISABLED,
                     "다음",
                     {
-                        if (!loginElderViewModel.nameList.filter { it.isNotEmpty() }
+                        if (!loginElderViewModel.elderDataList.filter { it.name.isNotEmpty() }
                                 .all {
-                                    it.matches(Regex("^[가-힣a-zA-Z]*$"))
+                                    it.name.matches(Regex("^[가-힣a-zA-Z]*$"))
                                 }
                         )
                             coroutineScope.launch {
                                 snackBarState.showSnackbar(
                                     "이름을 다시 확인해주세요",
-                                    duration = SnackbarDuration.Short
+                                    duration = SnackbarDuration.Short,
                                 )
                             }
-                        else if (!loginElderViewModel.dateOfBirthList.filter { it.isNotEmpty() }
+                        else if (!loginElderViewModel.elderDataList.filter { it.birthDate.isNotEmpty() }
                                 .all {
-                                    it.isValidDate()
+                                    it.birthDate.isValidDate()
                                 })
                             coroutineScope.launch {
                                 snackBarState.showSnackbar(
                                     "생년월일을 다시 확인해주세요",
-                                    duration = SnackbarDuration.Short
+                                    duration = SnackbarDuration.Short,
                                 )
                             }
-                        else if (!loginElderViewModel.phoneNumberList.filter { it.isNotEmpty() }
-                                .all { it.startsWith("010") })
+                        else if (!loginElderViewModel.elderDataList.filter { it.phoneNumber.isNotEmpty() }
+                                .all { it.phoneNumber.startsWith("010") })
                             coroutineScope.launch {
                                 snackBarState.showSnackbar(
                                     "휴대폰 번호를 다시 확인해주세요",
-                                    duration = SnackbarDuration.Short
+                                    duration = SnackbarDuration.Short,
                                 )
                             }
                         else {
-                            loginElderViewModel.createElderDataList()
                             navController.navigate(Route.LoginElderMedInfoScreen.route)
                         }
                     },
-                    modifier.padding(bottom = 20.dp)
+                    modifier.padding(bottom = 20.dp),
                 )
             }
         }
+
         DefaultSnackBar(
             snackBarState,
             Modifier
                 .align(Alignment.BottomCenter)
-                .padding(bottom = 14.dp)
+                .padding(bottom = 14.dp),
         )
     }
-
 }
