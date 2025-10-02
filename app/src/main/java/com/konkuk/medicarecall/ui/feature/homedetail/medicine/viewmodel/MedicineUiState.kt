69363bc57e1cee9ef377437b068066a2d3e31460
librawish808 <sophiadiary842@gmail.com>
2025-10-03 02:24:33 +0900

    Feat: #130 Medicine 상세 화면 UI 수정

--- app/src/main/java/com/konkuk/medicarecall/ui/feature/homedetail/medicine/viewmodel/MedicineUiState.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/feature/homedetail/medicine/viewmodel/MedicineUiState.kt
@@ -2,7 +2,6 @@ package com.konkuk.medicarecall.ui.feature.homedetail.medicine.viewmodel
 
 data class MedicineUiState(
     val medicineName: String,           // 약 이름
-    val todayTakenCount: Int,           // 오늘 복약 완료 횟수
     val todayRequiredCount: Int,        // 목표 복약 횟수
     // val nextDoseTime: String?,          // 다음 복약 예정 시간대 (MORNING 등)
     val doseStatusList: List<DoseStatusItem> // 복약 시간대 + 상태
@@ -10,7 +9,6 @@ data class MedicineUiState(
     companion object {
         val EMPTY = MedicineUiState(
             medicineName = "",
-            todayTakenCount = 0,
             todayRequiredCount = 0,
             // nextDoseTime = null,
             doseStatusList = emptyList()
