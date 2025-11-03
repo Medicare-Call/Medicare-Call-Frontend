8d797bb0975506b6d81d2e5f502dda72f115943b
ProtossManse <ingan2473@gmail.com>
2025-11-04 01:55:59 +0900

    Fix: #165 MainBottomBar background 추가

--- app/src/main/java/com/konkuk/medicarecall/ui/navigation/component/MainBottomBar.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/navigation/component/MainBottomBar.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
@@ -49,10 +50,12 @@ internal fun MainBottomBar(
         exit = fadeOut() + slideOut { IntOffset(0, it.height) },
     ) {
         Box(
-            modifier = Modifier.border(
-                width = 1.dp,
-                color = gray1,
-            ),
+            modifier = Modifier
+                .background(MediCareCallTheme.colors.white)
+                .border(
+                    width = 1.dp,
+                    color = gray1,
+                ),
         ) {
             Column {
                 Row(
