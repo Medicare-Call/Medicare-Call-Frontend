6910921bd3d2bec1ef5d4bf5f3d421033e56978f
Chae Minji <162430621+alswlekk@users.noreply.github.com>
2025-11-14 11:22:10 +0900

    Fix: 로그아웃 후 다시 시작했을 때 전화번호 부분 초기화 안되는 거 해결

--- app/src/main/java/com/konkuk/medicarecall/ui/feature/settings/screen/MyDataSettingScreen.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/feature/settings/screen/MyDataSettingScreen.kt
@@ -1,5 +1,6 @@
 package com.konkuk.medicarecall.ui.feature.settings.screen
 
+import android.content.Intent
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -28,12 +29,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import com.konkuk.medicarecall.MainActivity
 import com.konkuk.medicarecall.R
 import com.konkuk.medicarecall.data.dto.response.MyInfoResponseDto
 import com.konkuk.medicarecall.ui.feature.settings.component.LogoutConfirmDialog
@@ -59,6 +62,7 @@ fun MyDataSettingScreen(
         else -> "남성"
     }
     val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
 
     DisposableEffect(lifecycleOwner) {
         val obs = LifecycleEventObserver { _, event ->
@@ -186,6 +190,10 @@ fun MyDataSettingScreen(
                     onSuccess = {
                         Log.d("MyDataSettingScreen", "Logout successful")
                         showLogoutDialog = false
+                        val intent = Intent(context, MainActivity::class.java).apply {
+                            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        }
+                        context.startActivity(intent)
                         navigateToLoginAfterLogout() // 로그아웃 후 로그인 화면으로 이동
                     },
                     onError = { error ->
