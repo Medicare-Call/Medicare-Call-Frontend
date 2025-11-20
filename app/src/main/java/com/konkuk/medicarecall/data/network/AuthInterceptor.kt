c598fce3356b0920774fd12f9ab6952dabacb22d
ProtossManse <ingan2473@gmail.com>
2025-11-21 00:32:51 +0900

    Fix: #202 oldAccessToken 관련 문제 해결

--- app/src/main/java/com/konkuk/medicarecall/data/network/AuthInterceptor.kt
+++ app/src/main/java/com/konkuk/medicarecall/data/network/AuthInterceptor.kt
@@ -22,7 +22,7 @@ class AuthInterceptor @Inject constructor(
         // Interceptor는 백그라운드 스레드에서 실행되므로 runBlocking을 사용해도 UI를 차단하지 않음
         val accessToken = runBlocking {
             dataStoreRepository.getAccessToken()
-        }
+        } ?: ""
         // 4. 헤더에 AccessToken 추가하여 새로운 요청 생성
         val requestBuilder = originalRequest.newBuilder()
             .header("Authorization", "Bearer $accessToken")
