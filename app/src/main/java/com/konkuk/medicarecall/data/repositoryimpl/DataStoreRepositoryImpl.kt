5a56d5d0b333befeb460fe0b10f36ae786e620ae
Chae Minji <162430621+alswlekk@users.noreply.github.com>
2025-11-20 14:22:40 +0900

    Fix: PR 관련 수정사항 반영

--- app/src/main/java/com/konkuk/medicarecall/data/repositoryimpl/DataStoreRepositoryImpl.kt
+++ app/src/main/java/com/konkuk/medicarecall/data/repositoryimpl/DataStoreRepositoryImpl.kt
@@ -14,10 +14,6 @@ import javax.inject.Singleton
 val Context.tokenDataStore by dataStore(
     fileName = "tokens",
     serializer = TokenSerializer,
-    corruptionHandler = ReplaceFileCorruptionHandler {
-        // 기존 암호문이 깨졌을 때 기본값으로 복원
-        Token(null, null)
-    },
 )
 
 @Singleton
