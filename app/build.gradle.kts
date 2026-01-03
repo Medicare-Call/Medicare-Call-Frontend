a103eba0abbba4ad3fda7dfc9ee73eb909297db0
ProtossManse <ingan2473@gmail.com>
2026-01-03 12:02:48 +0900

    Chore: #210 코틀린 버전 2.0.21 -> 2.2.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
-    kotlin("plugin.serialization") version "2.0.21"
+    kotlin("plugin.serialization") version "2.2.20"
     alias(libs.plugins.detekt)
     alias(libs.plugins.google.services)
 }
