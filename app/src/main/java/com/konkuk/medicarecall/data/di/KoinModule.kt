a484ff6ac363aba0ef5ac40e29987faa43f75953
ProtossManse <ingan2473@gmail.com>
2026-01-03 10:10:45 +0900

    Refactor: #210 remove unused import

--- app/src/main/java/com/konkuk/medicarecall/data/di/KoinModule.kt
+++ app/src/main/java/com/konkuk/medicarecall/data/di/KoinModule.kt
@@ -35,9 +35,7 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
-import org.koin.core.module.dsl.singleOf
 import org.koin.core.qualifier.named
-import org.koin.dsl.bind
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
