6664c4da1c9a566c63dc6317a3e73f838459dcad
ProtossManse <ingan2473@gmail.com>
2026-01-03 11:52:42 +0900

    Chore: #210 Koin 의존성 중복 삭제 및 버전업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-import java.lang.module.ModuleFinder.compose
 import java.util.Properties
 
 plugins {
@@ -106,11 +105,6 @@ dependencies {
 
     // WebView
     implementation("com.google.accompanist:accompanist-webview:0.24.13-rc")
-
-    // Koin
-    implementation("io.insert-koin:koin-core:3.5.3")
-    implementation("io.insert-koin:koin-android:3.5.3")
-    implementation("io.insert-koin:koin-androidx-compose:3.5.3")
 
     // Koin
     implementation(libs.koin.android)
