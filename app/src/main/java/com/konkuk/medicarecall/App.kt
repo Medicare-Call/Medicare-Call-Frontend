1d5ee00b812ae2e4114ec1d9d9fa206de7ed11dd
ProtossManse <ingan2473@gmail.com>
2026-01-03 12:10:03 +0900

    Refactor: #210 KoinApplication 적용

--- app/src/main/java/com/konkuk/medicarecall/App.kt
+++ app/src/main/java/com/konkuk/medicarecall/App.kt
@@ -18,14 +18,16 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
+import org.koin.core.annotation.KoinApplication
 import org.koin.core.context.startKoin
 import org.koin.ksp.generated.defaultModule
 
-class App : Application(), KoinComponent {
+
+@KoinApplication
+class App : Application() {
 
     private val fcmRepository: FcmRepository by inject()
 
