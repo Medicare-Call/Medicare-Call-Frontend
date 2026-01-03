b62284e701b1da2ec7c50f53a7335bb40733f398
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-03 18:24:44 +0900

    Chore: #210 린트 수정

--- app/src/main/java/com/konkuk/medicarecall/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/navigation/NavGraph.kt
@@ -3,7 +3,6 @@ package com.konkuk.medicarecall.ui.navigation
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
