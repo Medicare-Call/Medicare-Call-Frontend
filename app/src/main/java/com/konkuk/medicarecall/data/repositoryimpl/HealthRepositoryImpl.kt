51e678fd07370650ce9a499bc8e34f1acb07d15f
librawish808 <sophiadiary842@gmail.com>
2026-01-08 19:20:55 +0900

    Refactor: #216 HealthRepositoryImpl에서 null-safe 로직으로 개선

--- app/src/main/java/com/konkuk/medicarecall/data/repositoryimpl/HealthRepositoryImpl.kt
+++ app/src/main/java/com/konkuk/medicarecall/data/repositoryimpl/HealthRepositoryImpl.kt
@@ -16,7 +16,9 @@ class HealthRepositoryImpl(
             HealthUiState(
                 symptoms = response.symptomList.orEmpty(),
                 symptomAnalysis = response.analysisComment.orEmpty(),
-                isRecorded = response.symptomList!!.isNotEmpty() || !response.analysisComment.isNullOrBlank(),
+                isRecorded =
+                    !response.symptomList.isNullOrEmpty() ||
+                        !response.analysisComment.isNullOrBlank()
             )
         }
 }
