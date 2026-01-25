6f6c8314227152c0819540455abc13b7103e173d
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-26 03:43:10 +0900

    Refactor: #224 API 응답 핸들러 적용
    - `HealthRepositoryImpl`에서 `handleResponse()` 유틸 함수를 사용하여 API 응답을 처리하도록 수정
    - 코드 중복을 줄이고 일관성 있는 오류 처리 방식 적용

--- app/src/main/java/com/konkuk/medicarecall/data/repositoryimpl/HealthRepositoryImpl.kt
+++ app/src/main/java/com/konkuk/medicarecall/data/repositoryimpl/HealthRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.konkuk.medicarecall.data.repositoryimpl
 
 import com.konkuk.medicarecall.data.api.elders.HealthService
 import com.konkuk.medicarecall.data.repository.HealthRepository
+import com.konkuk.medicarecall.data.util.handleResponse
 import com.konkuk.medicarecall.ui.feature.homedetail.statehealth.viewmodel.HealthUiState
 import org.koin.core.annotation.Single
 import java.time.LocalDate
@@ -12,7 +13,7 @@ class HealthRepositoryImpl(
 ) : HealthRepository {
     override suspend fun getHealthUiState(elderId: Int, date: LocalDate): Result<HealthUiState> =
         runCatching {
-            val response = healthService.getDailyHealth(elderId, date.toString())
+            val response = healthService.getDailyHealth(elderId, date.toString()).handleResponse()
             HealthUiState(
                 symptoms = response.symptomList.orEmpty(),
                 symptomAnalysis = response.analysisComment.orEmpty(),
