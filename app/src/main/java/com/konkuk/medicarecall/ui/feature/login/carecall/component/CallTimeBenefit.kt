83013e7d16077879fbf9b82eca35833c6830c050
librawish808 <sophiadiary842@gmail.com>
2026-01-28 00:48:32 +0900

    Chore: #225 임포트 추가

--- app/src/main/java/com/konkuk/medicarecall/ui/feature/login/carecall/component/CallTimeBenefit.kt
+++ app/src/main/java/com/konkuk/medicarecall/ui/feature/login/carecall/component/CallTimeBenefit.kt
@@ -1,8 +1,11 @@
 package com.konkuk.medicarecall.ui.feature.login.carecall.component
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
