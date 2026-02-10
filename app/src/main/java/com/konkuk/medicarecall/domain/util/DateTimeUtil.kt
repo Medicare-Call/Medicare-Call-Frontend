1e170a3a97b4c54107726bb3f538fad85a266f86
ProtossManse <ingan2473@gmail.com>
2026-02-11 00:42:40 +0900

    Feat: #255 datetime 관련 확장 함수 및 util 코드 추가

---
+++ app/src/main/java/com/konkuk/medicarecall/domain/util/DateTimeUtil.kt
@@ -0,0 +1,38 @@
+package com.konkuk.medicarecall.domain.util
+
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.isoDayNumber
+import kotlinx.datetime.minus
+import kotlinx.datetime.plus
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.todayIn
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+
+val LocalDate.Companion.MIN: LocalDate
+    get() = LocalDate(-999_999_999, 1, 1)
+
+val LocalDate.Companion.MAX: LocalDate
+    get() = LocalDate(999_999_999, 12, 31)
+
+@OptIn(ExperimentalTime::class)
+fun LocalDate.Companion.now(): LocalDate =
+    Clock.System.todayIn(TimeZone.currentSystemDefault())
+
+@OptIn(ExperimentalTime::class)
+fun LocalDateTime.Companion.now(): LocalDateTime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+
+fun LocalDate.weekStart(): LocalDate {
+    val daysFromMonday = this.dayOfWeek.isoDayNumber - 1
+    return this.minus(daysFromMonday, DateTimeUnit.DAY)
+}
+
+fun LocalDate.getWeekRange(): Pair<LocalDate, LocalDate> {
+    val start = this.weekStart()
+    val end = start.plus(6, DateTimeUnit.DAY) // 월요일 + 6일 = 일요일
+
+    return start to end
+}
